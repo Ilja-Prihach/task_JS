01a4063f99605e828d0b862f1515447a541839de
Ilja-Prihach <piratehnik9508@gmail.com>
2022-09-25 18:21:03 +0300

    new task:metodi i massivi

--- JS/script.js
+++ JS/script.js
@@ -215,3 +215,60 @@ calculateVolumeAndArea(6);
     return Math.ceil(roomate);
 }
 getCoupeNumber (13); */
+
+
+// Task JS
+
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+/*     3) Создайте метод showAgeAndLangs внутри объекта personalPlanPeter.
+ При его вызове метод будет принимать в себя объект и возвращать строку в нужном виде.
+Пример:
+personalPlanPeter.showAgeAndLangs(personalPlanPeter)
+=> 'Мне 29 и я владею языками: RU ENG' */
+    showAgeAndLangs: function(plan) {
+        const {age} = plan;
+        const {languages} = plan.skills;
+        let str = `Мне ${age} и я владею языками: `;
+
+        languages.forEach(function(lang) {
+            str += `${lang.toUpperCase()} `;
+        });
+
+        return str;
+    }
+};
+
+//1) Напишите функцию showExperience, которая будет принимать в себя объект со
+// всеми данными и возвращать строку с опытом.
+
+function showExperience(plan) {
+    const {exp} = plan.skills;
+    return exp;
+}
+
+/* 2) Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со
+ всеми данными и возвращать строку в нужном виде.
+Пример:
+showProgrammingLangs(personalPlanPeter)  =>
+"Язык js изучен на 20% Язык php изучен на 10%"
+Причем функция должна работать вне зависимости от количества языков.
+Если ни один не указан, то возвращается пустая строка. */
+
+function showProgrammingLangs(plan) {
+    let str = '';
+    const {programmingLangs} = plan.skills;
+    for (let key in programmingLangs) {
+        str += `Язык ${key} изучен на ${programmingLangs[key]}/n`;
+    }
+    return str;
+}
